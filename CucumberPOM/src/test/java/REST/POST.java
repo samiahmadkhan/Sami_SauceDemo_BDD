d031293ff83bbfd3c2e241a67af951fefafbc84f
Sami Ahmad Khan <samikhan78666@gmail.com>
2023-10-24 00:22:19 +0530

    added REST APIs GET and POST

---
+++ CucumberPOM/src/test/java/REST/POST.java
@@ -0,0 +1,27 @@
+package REST;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.json.JSONObject;
+import org.json.JSONString;
+import org.testng.annotations.Test;
+
+public class POST {
+	@Test
+	public void addUser() {
+		JSONObject request=new JSONObject();
+		request.put("name","morpheus");
+		request.put("job","leader");
+		
+		System.out.println(request.toString());
+		
+		given().
+		body(request.toString()).log().all().
+		when().post("https://reqres.in/api/users").
+		then().
+		     statusCode(201).log().all();
+		     
+		    
+	}
+}
--- CucumberPOM/src/test/java/Testrunner/myTestRunner.java
+++ CucumberPOM/src/test/java/Testrunner/myTestRunner.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
+//i have added sparks report which will be generate in locations: CucumberPOM/test-output/SparkReport
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
 			features = {"src/test/resource/features"},
 			glue = {"stepDefinations", "hooks"},
-            tags = "@test",
+            tags = "@REST",
             monochrome = true,
 			plugin = {"pretty",
 					"json:target/MyReports/report.json",
