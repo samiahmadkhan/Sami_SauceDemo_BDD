f78c166ec07321ad281e5aab50ec0af5d66fa941
Sami Ahmad Khan <samikhan@Samis-MBP.lan>
2024-07-11 17:41:37 +0530

    added many new test

--- src/main/java/com/factory/driverFactory.java
+++ src/main/java/com/factory/driverFactory.java
@@ -7,10 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class driverFactory {
      public WebDriver driver;
-	 public static ThreadLocal<WebDriver> tDriver=new ThreadLocal<>();
+	 private static ThreadLocal<WebDriver> tDriver=new ThreadLocal<>();
      public WebDriver initializeDriver(String browser) {
     	 System.out.println("Browser is = " + browser);
     	 if(browser.equalsIgnoreCase("Chrome")) {
@@ -19,14 +20,12 @@ public class driverFactory {
     		// System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/browserDrivers/chromedriver");
 			 tDriver.set(new ChromeDriver());
     	 }else if(browser.equalsIgnoreCase("Edge")) {
-    		 WebDriverManager.edgedriver().setup();
-    		 driver=new EdgeDriver();
-    		
-    	 }else System.out.println("No Browser Exists");
+			 tDriver.set(new EdgeDriver());
+    	 }else if(browser.equalsIgnoreCase("Safari")) {
+		tDriver.set(new SafariDriver());
+	}else System.out.println("No Browser Exists");
     	 getDriver().manage().window().maximize();
 		 return getDriver();
- 		 
-    	 
      }
      
      public static synchronized WebDriver getDriver() {
